d8ee3171e7ab653e970e0f3088b70b00e8121743
Praveen MP <praveenmp490@gmail.com>
2025-12-29 20:33:54 +0530

    other recursions problems added

--- arrays/recursions.py
+++ arrays/recursions.py
@@ -67,3 +67,81 @@ target = 8
 left = 0
 right = len(arr) - 1
 print(binary_search(arr, left, right, target))
+
+# Reverse of the number
+
+def reversing(n, rev=0):
+    if n == 0:
+        return rev
+    return reversing(n // 10, rev=rev * 10 + n % 10)
+
+
+print(reversing(12345))
+
+
+# Reversing string
+
+def string_reverse(s):
+    if len(s) <= 1:
+        return s
+    return string_reverse(s[1:]) + s[0]
+
+
+print(string_reverse('hello there'))
+
+
+# Sum of digits
+
+def digit_sum(n):
+    if n <= 1:
+        return 1
+    return n % 10 + digit_sum(n // 10)
+
+
+print(digit_sum(123))
+
+
+# count of digits
+
+def count_digit(n):
+    if n == 0:
+        return 0
+    return 1 + count_digit(n // 10)
+
+
+print(count_digit(2234))
+
+
+# First n natural numbers sum
+
+def natural(n):
+    if n == 0:
+        return n
+    return n + natural(n - 1)
+
+
+print(natural(5))
+
+
+# Print first 10 numbers
+
+def ten(n, start=1):
+    if start == n + 1:
+        return
+    print(start)
+    return ten(n, start + 1)
+
+
+ten(10)
+
+
+# Print from 10 to 1
+
+def number(n):
+    if n == 0:
+        return
+    print(n)
+    return number(n - 1)
+
+
+number(10)
