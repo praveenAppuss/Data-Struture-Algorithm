fb4e44bfce28412efc9c1d3d7a38808749f8fc7a
Praveen MP <praveenmp490@gmail.com>
2025-12-29 20:31:55 +0530

    recursion of large and second numbers added

--- arrays/recursions.py
+++ arrays/recursions.py
@@ -12,3 +12,38 @@ def anagram(s1, s2):
     s1 = s1.replace(m1, '')
     s2 = s2.replace(m2, '')
     return anagram(s1, s2)
+
+print(anagram('hello', 'oellh'))
+
+# FInd the second largest element
+
+arr = [1, 2, 3, 4, 5, 6, 7, 88, 44, 33, 56, 66]
+
+
+def sec_largest(arr, index=0, large=0, sec=0):
+    if index == len(arr):
+        return sec
+    if arr[index] > large:
+        sec = large
+        large = arr[index]
+    elif arr[index] > sec and arr[index] != large:
+        sec = arr[index]
+    return sec_largest(arr, index + 1, large, sec)
+
+
+print(sec_largest(arr))
+
+# Find large element
+
+arr = [1, 2, 3, 4, 5, 6, 7, 88, 44, 33, 56, 66]
+
+
+def largerst_element(arr, index=0, large=0):
+    if index == len(arr):
+        return large
+    if arr[index] > large:
+        large = arr[index]
+    return largerst_element(arr, index + 1, large)
+
+
+print(largerst_element(arr))
